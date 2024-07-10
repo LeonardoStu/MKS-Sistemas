6e32ae83221254dda8e4822342bbfed1800c63f1
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-07-10 03:45:08 -0300

    Feito o front-end do carrinho de compras

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -5,44 +5,67 @@ import style from './style.module.scss'
 import { Product, useCartStore } from '@/store/useCartStore'
 import { FaCircleXmark } from 'react-icons/fa6'
 
-export function Cart() {
+interface CartProps {
+    onClose: () => void
+}
+
+export function Cart({onClose}: CartProps) {
     const [cart, increaseProductQuantity, decreaseQuantity, removeFromCart, getTotalPrice] = useCartStore((state) => [state.cart, state.increaseQuantity, state.decreaseQuantity, state.removeFromCart, state.getTotalPrice()])
 
-    return<>
-        <div className={style.container}>
-            {
-                cart.length > 0 ? (
-                    cart.map((product: Product) => (
-                        <div key={product.id} className={style.productContainer}>
-                            <div>
-                                <Image width={50} height={50} src={product.photo} alt={product.name} className={style.productImg}/>
-                            </div>
+    return(
+        <div className={style.cartContainer}>
+            <div className={style.header}>
+                <h1>Carrinho de Compras</h1>
+                <button onClick={onClose} className={style.closeBtn}>
+                    <FaCircleXmark style={{width:'100%', height:'50px', color:'black'}}/>
+                </button>
+            </div>
+            <div className={style.content}>
+                {
+                    cart.length > 0 ? (
+                        cart.map((product: Product) => (
+                            <div key={product.id} className={style.productContainer}>
+                                <div style={{width:'100px'}}>
+                                    <Image width={200} height={200} src={product.photo} alt={product.name} className={style.productImg}/>
+                                </div>
 
-                            <div>
-                                <p>{product.name}</p>
-                            </div>
+                                <div style={{fontSize:'20px', width:'100px'}}>
+                                    <p>{product.name}</p>
+                                </div>
 
-                            <div>
-                                <p>Qtd</p>
-                                <button onClick={() => decreaseQuantity(product.id)}>-</button>
-                                <p>{product.quantity}</p>
-                                <button onClick={() => increaseProductQuantity(product.id)}>+</button>
-                            </div>
+                                <div style={{display:'flex', flexDirection:'column'}}>
+                                    <div>
+                                        <p style={{fontSize:'12px'}}>Qtd</p>
+                                    </div>
+                                    <div className={style.btnQuantity}>
+                                        <button onClick={() => decreaseQuantity(product.id)} className={style.quantityBtn}>-</button>
+                                        <p style={{margin:'0 5px 0 5px'}}>{product.quantity}</p>
+                                        <button onClick={() => increaseProductQuantity(product.id)} className={style.quantityBtn}>+</button>
+                                    </div>
+                                </div>
 
-                            <div>
-                                <p>R${product.price}</p>
+                                <div style={{fontSize:'25px', marginRight:'45px', fontWeight:'bold'}}>
+                                    <p>R${product.price}</p>
+                                </div>
+                                <button onClick={() => removeFromCart(product.id)} className={style.removeButton}>
+                                    <FaCircleXmark />
+                                </button>
                             </div>
-                            <button onClick={() => removeFromCart(product.id)}>
-                                <FaCircleXmark />
-                            </button>
-                        </div>
-                    ))
-                ) : (
-                    <h1>Carrinho vazio!</h1>
-                )
-            }
-
-            <h1>Total do carrinho {getTotalPrice.toFixed(2)}</h1>
+                        ))
+                    ) : (
+                        <h1 style={{color:'white', textAlign:'center'}}>Carrinho vazio!</h1>
+                    )
+                }
+            </div>
+            <div className={style.footer}>
+                <div style={{display:'flex', justifyContent:'space-between', width:'90%', color:'white', alignItems:"center", padding:'10px'}}>
+                    <h1>Total:</h1>
+                    <p style={{fontSize:'25px', marginRight:'45px', fontWeight:'bold'}}>R${getTotalPrice.toFixed(2)}</p>
+                </div>
+                <div style={{width:'100%'}}>
+                    <button className={style.finishBuy}>Finalizer Compra</button>
+                </div>
+            </div>
         </div>
-    </>
+    )
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,25 @@
 "use client"
 
 import style from './style.module.scss'
+import { Cart } from '../cart'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 import { FaCartShopping } from 'react-icons/fa6'
+import { useCartStore } from '@/store/useCartStore'
 
 export default function Header() {
+    const [isVisible, setisVisible] = useState(false)
+    const cart = useCartStore(state => state.cart)
+
+    const toggleCart = () => {
+        setisVisible(!isVisible)
+    }
+
+    const cartVariants = {
+        hidden: {x: '100%'},
+        visible: {x: 0}
+    }
+
     return<>
         <div className={style.header}>
             <div style={{display: 'flex', alignItems: 'center', color: 'white'}}>
@@ -11,10 +27,19 @@ export default function Header() {
                 <span style={{paddingLeft: '10px', fontSize: '20px'}}>Sistemas</span>
             </div>
             <div>
-                <button className={style.btnCart}>
-                    <FaCartShopping className={style.iconCart}/> <p style={{fontSize: '20px', paddingLeft: '15px'}}>0</p>
+                <button className={style.btnCart} onClick={toggleCart}>
+                    <FaCartShopping className={style.iconCart}/> <p style={{fontSize: '20px', paddingLeft: '15px'}}>{cart.length}</p>
                 </button>
             </div>
         </div>
+        <motion.div
+        initial="hidden"
+        animate={isVisible ? "visible" : "hidden"}
+        variants={cartVariants}
+        transition={{ type: 'spring', stiffness: 100 }}
+        className={style.cartContainer}
+        >
+            {isVisible && <Cart onClose={toggleCart}/>}
+        </motion.div>
     </>
 }
